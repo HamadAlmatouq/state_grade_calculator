ebdf5a52ad301c465c413d0b02312855e101a9ef
Hamad Almatouq <hamad.almatouq@outlook.com>
2024-10-13 11:41:36 +0300

    -Adds a display for the grade -Adds calculateGrade funcation -Adds if statement

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final textController = TextEditingController();
+  String result = "";
+
+  String calculateGrade(double score) {
+    if (score >= 90) {
+      return "A";
+    } else if (score >= 80) {
+      return "B";
+    } else if (score >= 70) {
+      return "C";
+    } else if (score >= 60) {
+      return "D";
+    } else {
+      return "F";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +48,7 @@ class HomeScreen extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        backgroundColor: Colors.pink,
+        backgroundColor: Colors.pink.shade200,
         centerTitle: true,
       ),
       body: SafeArea(
@@ -34,8 +56,9 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             children: [
               Container(
-                padding: EdgeInsets.all(40),
+                padding: EdgeInsets.all(60),
                 child: TextField(
+                  controller: textController,
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
                     hintText: "Type your score",
@@ -44,15 +67,34 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               Container(
+                padding: EdgeInsets.only(bottom: 30),
                 child: ElevatedButton(
-                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.pink.shade200,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      double score = double.parse(textController.text);
+
+                      result = calculateGrade(score);
+                    });
+                  },
                   child: Text(
                     "Calculate",
                     style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                        fontSize: 15),
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 15,
+                    ),
                   ),
+                ),
+              ),
+              Text(
+                result,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                  fontSize: 30,
                 ),
               ),
             ],
